4b9fe7c110a3abb9b3920613eb780186050e6f30
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2026-05-26 00:03:24 -0300

    first commit

---
+++ src/main/java/com/ordep/aspmanagergateway/AspmanagerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ordep.aspmanagergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspmanagerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AspmanagerGatewayApplication.class, args);
+    }
+
+}
